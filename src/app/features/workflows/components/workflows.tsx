09ad5c0ab5ef352191ebc29f83cadbf1d798fd83
Tushar Gagal <tushargagal1209@gmail.com>
2026-02-04 16:05:51 +0530

    11: workflow pagination and searching

--- src/app/features/workflows/components/workflows.tsx
+++ src/app/features/workflows/components/workflows.tsx
@@ -1,54 +1,81 @@
 "use client"
-import { EntityContainer, EntityHeader } from "@/components/entity-components";
+import { EntityContainer, EntityHeader, EntityPagination, EntitySearch } from "@/components/entity-components";
 import { useCreateWorkflows, useSuspenseWorkflows } from "../hooks/useWorkflows"
 import { useRouter } from "next/navigation";
+import { useWorkflowsParams } from "../hooks/useWorkflows-params";
+import { useEntitySearch } from "@/hooks/use-entity-search";
 
-export const WorkflowList=()=>{
-    const workflows=useSuspenseWorkflows();
+export const WorkflowsSearch = () => {
+    const [params, setParams] = useWorkflowsParams();
+    const { searchValue, onSearchChange } = useEntitySearch({ params, setParams });
+    return (
+        <EntitySearch
+            value={searchValue}
+            onChange={onSearchChange}
+            placeholder="Search Workflows"
+        />
+    )
+}
 
-    return(
+export const WorkflowList = () => {
+    const workflows = useSuspenseWorkflows();
+
+    return (
         <p>
-            {JSON.stringify(workflows.data,null,2)}
+            {JSON.stringify(workflows.data, null, 2)}
         </p>
     )
 }
 
-export const WorkflowsHeader=({disabled}:{disabled?:boolean})=>{
-    const createWorkflow=useCreateWorkflows();
+export const WorkflowsHeader = ({ disabled }: { disabled?: boolean }) => {
+    const createWorkflow = useCreateWorkflows();
     const router = useRouter();
 
-    const handleCreateWorkflow=()=>{
-        createWorkflow.mutate(undefined,{
-            onSuccess:(data)=>{
+    const handleCreateWorkflow = () => {
+        createWorkflow.mutate(undefined, {
+            onSuccess: (data) => {
                 router.push(`/workflows/${data.id}`)
             },
-            onError:(error)=>{
+            onError: (error) => {
                 // If we add Paid subscription then we can add here accordingly.
                 console.log(error)
             }
         });
     }
-    return(
+    return (
         <>
             <EntityHeader
-            title="Workflows"
-            description="Create and manage your workflows"
-            onNew={handleCreateWorkflow}
-            // newButtonHref="123"
-            newButtonLabel="New Workflow"
-            disabled={disabled}
-            isCreating={createWorkflow.isPending}
+                title="Workflows"
+                description="Create and manage your workflows"
+                onNew={handleCreateWorkflow}
+                // newButtonHref="123"
+                newButtonLabel="New Workflow"
+                disabled={disabled}
+                isCreating={createWorkflow.isPending}
             />
         </>
     )
 }
 
-export const WorkflowsContainer =({children}:{children:React.ReactNode})=>{
-    return(
+export const WorkflowsPagination = () => {
+    const workflows = useSuspenseWorkflows();
+    const [params, setParams] = useWorkflowsParams();
+    return (
+        <EntityPagination
+            disabled={workflows.isFetching}
+            totalPages={workflows.data.totalPages}
+            page={workflows.data.page}
+            onPageChange={(page) => setParams({ ...params, page })}
+        />
+    )
+}
+
+export const WorkflowsContainer = ({ children }: { children: React.ReactNode }) => {
+    return (
         <EntityContainer
-            header={<WorkflowsHeader/>}
-            search={<></>}
-            pagination={<></>}
+            header={<WorkflowsHeader />}
+            search={<WorkflowsSearch />}
+            pagination={<WorkflowsPagination />}
         >
             {children}
         </EntityContainer>
